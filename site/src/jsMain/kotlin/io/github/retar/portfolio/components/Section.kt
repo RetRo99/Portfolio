a1a6ebae498494c71124a9074c016a54e7c0f270
Rok Retar <retar.rok@gmail.com>
2025-10-30 08:11:12 +0100

    Create reusable PortfolioSection component

---
+++ site/src/jsMain/kotlin/io/github/retar/portfolio/components/Section.kt
@@ -0,0 +1,32 @@
+package io.github.retar.portfolio.components
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.ColumnScope
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.compose.ui.modifiers.width
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.toModifier
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
+
+@Composable
+fun PortfolioSection(
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column(
+        modifier = SectionContainerStyle.toModifier(),
+        content = content
+    )
+}
+
+val SectionContainerStyle = CssStyle {
+    base {
+        Modifier
+            .padding(topBottom = 32.px, leftRight = 16.px)
+            .width(100.percent)
+            .maxWidth(1200.px)
+    }
+}
