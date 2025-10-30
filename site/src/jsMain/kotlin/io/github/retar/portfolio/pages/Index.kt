b16795799a538f2d8c1e616f861d3473ba228295
Rok Retar <rok.retar@devrev.ai>
2025-10-30 08:24:55 +0100

    Refactor(Index): Remove unused CardStyle

--- site/src/jsMain/kotlin/io/github/retar/portfolio/pages/Index.kt
+++ site/src/jsMain/kotlin/io/github/retar/portfolio/pages/Index.kt
@@ -6,10 +6,6 @@ import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.alignItems
-import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
-import com.varabyte.kobweb.compose.ui.modifiers.border
-import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
-import com.varabyte.kobweb.compose.ui.modifiers.boxShadow
 import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.compose.ui.modifiers.fontSize
@@ -21,7 +17,6 @@ import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.text.SpanText
 import com.varabyte.kobweb.silk.style.CssStyle
 import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
-import com.varabyte.kobweb.silk.style.selectors.hover
 import com.varabyte.kobweb.silk.style.toModifier
 import io.github.retar.portfolio.components.PortfolioButton
 import io.github.retar.portfolio.components.PortfolioSection
@@ -30,10 +25,7 @@ import org.jetbrains.compose.web.css.AlignItems
 import org.jetbrains.compose.web.css.CSSSizeValue
 import org.jetbrains.compose.web.css.CSSUnit
 import org.jetbrains.compose.web.css.Color
-import org.jetbrains.compose.web.css.LineStyle
 import org.jetbrains.compose.web.css.px
-import org.jetbrains.compose.web.css.rgb
-import org.jetbrains.compose.web.css.rgba
 import org.jetbrains.compose.web.dom.H1
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
@@ -63,16 +55,6 @@ val DescriptorStyle = CssStyle {
             .color(Color.lightgray)
             .fontSize(18.px)
     }
-}
-val CardStyle = CssStyle {
-    base {
-        Modifier
-            .padding(20.px)
-            .borderRadius(12.px)
-            .border(1.px, LineStyle.Solid, rgba(0, 0, 0, 0.1))
-            .backgroundColor(rgb(250, 250, 250))
-    }
-    hover { Modifier.boxShadow(0.px, 8.px, 24.px, 0.px, rgba(0, 0, 0, 0.12)) }
 }
 
 @Page
