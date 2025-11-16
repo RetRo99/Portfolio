857250707022c34dbe17e0263bc1fe2c113cdb7f
Rok Retar <rok.retar@devrev.ai>
2025-11-16 19:58:56 +0100

    Refactor: Extract typography styles to a separate file

--- site/src/jsMain/kotlin/io/github/retar/portfolio/pages/Index.kt
+++ site/src/jsMain/kotlin/io/github/retar/portfolio/pages/Index.kt
@@ -1,61 +1,31 @@
 package io.github.retar.portfolio.pages
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.alignItems
-import com.varabyte.kobweb.compose.ui.modifiers.color
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
-import com.varabyte.kobweb.compose.ui.modifiers.fontSize
-import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
 import com.varabyte.kobweb.compose.ui.modifiers.gap
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.text.SpanText
-import com.varabyte.kobweb.silk.style.CssStyle
-import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.style.toModifier
 import io.github.retar.portfolio.components.PortfolioButton
 import io.github.retar.portfolio.components.PortfolioSection
 import io.github.retar.portfolio.resources.StringRes
+import io.github.retar.portfolio.styles.TitleStyle
+import io.github.retar.portfolio.styles.SubtitleStyle
+import io.github.retar.portfolio.styles.DescriptorStyle
 import org.jetbrains.compose.web.css.AlignItems
 import org.jetbrains.compose.web.css.CSSSizeValue
 import org.jetbrains.compose.web.css.CSSUnit
-import org.jetbrains.compose.web.css.Color
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.H1
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 
-val TitleStyle = CssStyle {
-    base {
-        Modifier
-            .fontSize(55.px)
-            .fontWeight(FontWeight.Normal)
-    }
-
-    Breakpoint.ZERO {
-        Modifier.fontSize(32.px)
-    }
-}
-
-val SubtitleStyle = CssStyle {
-    base {
-        Modifier
-            .fontSize(18.px)
-    }
-}
-
-val DescriptorStyle = CssStyle {
-    base {
-        Modifier
-            .color(Color.lightgray)
-            .fontSize(18.px)
-    }
-}
 
 @Page
 @Composable
---
+++ site/src/jsMain/kotlin/io/github/retar/portfolio/styles/Typography.kt
@@ -0,0 +1,38 @@
+package io.github.retar.portfolio.styles
+
+import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
+import org.jetbrains.compose.web.css.Color
+import org.jetbrains.compose.web.css.px
+
+val TitleStyle = CssStyle {
+    base {
+        Modifier
+            .fontSize(55.px)
+            .fontWeight(FontWeight.Normal)
+    }
+
+    Breakpoint.ZERO {
+        Modifier.fontSize(32.px)
+    }
+}
+
+val SubtitleStyle = CssStyle {
+    base {
+        Modifier
+            .fontSize(18.px)
+    }
+}
+
+val DescriptorStyle = CssStyle {
+    base {
+        Modifier
+            .color(Color.lightgray)
+            .fontSize(18.px)
+    }
+}
