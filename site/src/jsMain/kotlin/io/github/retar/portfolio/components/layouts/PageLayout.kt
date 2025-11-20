2aa7facbccb94401f41af3bf8bc3baad3380a3e1
Rok Retar <rok.retar@devrev.ai>
2025-11-20 20:10:33 +0100

    Refactor(PageLayout): Wrap content in Box with weight to push Footer down

--- site/src/jsMain/kotlin/io/github/retar/portfolio/components/layouts/PageLayout.kt
+++ site/src/jsMain/kotlin/io/github/retar/portfolio/components/layouts/PageLayout.kt
@@ -1,9 +1,11 @@
 package io.github.retar.portfolio.components.layouts
 
 import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.core.layout.Layout
 import io.github.retar.portfolio.components.footer.Footer
 import io.github.retar.portfolio.components.header.NavHeader
@@ -16,7 +18,13 @@ fun PageLayout(content: @Composable () -> Unit) {
             .fillMaxSize()
     ) {
         NavHeader()
-        content()
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            content()
+        }
         Footer()
     }
 }
