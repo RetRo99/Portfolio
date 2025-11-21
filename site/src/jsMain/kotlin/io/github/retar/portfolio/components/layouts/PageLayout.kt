22c40a9c725cb94f7e6b8eb44cc67d894d4c7e67
Rok Retar <rok.retar@devrev.ai>
2025-11-21 11:04:33 +0100

    Refactor(PageLayout): Make NavHeader sticky with white background

--- site/src/jsMain/kotlin/io/github/retar/portfolio/components/layouts/PageLayout.kt
+++ site/src/jsMain/kotlin/io/github/retar/portfolio/components/layouts/PageLayout.kt
@@ -5,15 +5,21 @@ import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.compose.ui.modifiers.position
+import com.varabyte.kobweb.compose.ui.modifiers.top
+import com.varabyte.kobweb.compose.ui.modifiers.zIndex
 import com.varabyte.kobweb.core.layout.Layout
 import com.varabyte.kobweb.silk.style.CssStyle
 import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.style.toModifier
 import io.github.retar.portfolio.components.footer.Footer
 import io.github.retar.portfolio.components.header.NavHeader
+import org.jetbrains.compose.web.css.Position
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 
@@ -51,7 +57,13 @@ fun PageLayout(content: @Composable () -> Unit) {
     ) {
 
         Box(
-            modifier = NavHeaderStyle.toModifier().fillMaxWidth(),
+            modifier = NavHeaderStyle
+                .toModifier()
+                .fillMaxWidth()
+                .position(Position.Sticky)
+                .top(0.px)
+                .zIndex(1)
+                .backgroundColor(Colors.White),
         ) {
             NavHeader()
         }
