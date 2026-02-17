acac03dca2d6b0e713e2e61c0d1dc51e87850e94
Rok Retar <rok.retar@devrev.ai>
2026-02-17 13:24:05 +0100

    Add 5 Parrot app screenshots to home page carousel
    - Renamed and moved 5 WhatsApp images to parrot directory
    - Added ParrotScreenshot1-5 entries to ImageRes enum
    - Updated SelectedProjects list to include all Parrot screenshots
    - Screenshots now display in home page project carousel

--- site/src/jsMain/kotlin/io/github/retar/portfolio/resources/ImageRes.kt
+++ site/src/jsMain/kotlin/io/github/retar/portfolio/resources/ImageRes.kt
@@ -6,7 +6,14 @@ enum class ImageRes(val path: String) {
     Bardy3("bardy/Bardy3.avif"),
 
     InfiniteCarouselArticle("articles/infinite_carousel.png"),
+
     ParrotIcon("parrot/parrot.png"),
+    ParrotScreenshot1("parrot/parrot-screenshot-1.jpeg"),
+    ParrotScreenshot2("parrot/parrot-screenshot-2.jpeg"),
+    ParrotScreenshot3("parrot/parrot-screenshot-3.jpeg"),
+    ParrotScreenshot4("parrot/parrot-screenshot-4.jpeg"),
+    ParrotScreenshot5("parrot/parrot-screenshot-5.jpeg"),
+
     ProfileImage("profile/profile_image.png"),
 }
 
--- site/src/jsMain/kotlin/io/github/retar/portfolio/resources/ProjectsRes.kt
+++ site/src/jsMain/kotlin/io/github/retar/portfolio/resources/ProjectsRes.kt
@@ -5,6 +5,11 @@ object ProjectsRes {
         ImageRes.Bardy1,
         ImageRes.Bardy2,
         ImageRes.Bardy3,
+        ImageRes.ParrotScreenshot1,
+        ImageRes.ParrotScreenshot2,
+        ImageRes.ParrotScreenshot3,
+        ImageRes.ParrotScreenshot4,
+        ImageRes.ParrotScreenshot5,
     )
 }
 
